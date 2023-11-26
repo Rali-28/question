6b309c1568a9b4f6a1fbb3e5fb356ca85d0ff2b2
Rali <113418936+Rali-28@users.noreply.github.com>
2023-11-26 17:34:06 +0800

    import useState

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React , { useState } from 'react'
 import ChanceButton from './ChanceButton'
 import ChanceMessage from './ChanceMessage'
 import Loader from './Loader'
