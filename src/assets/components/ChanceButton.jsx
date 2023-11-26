e3e1f22e53b70138c8153f5d9d70768f36f5a125
Rali <113418936+Rali-28@users.noreply.github.com>
2023-11-26 17:02:09 +0800

    Update button style

--- src/assets/components/ChanceButton.jsx
+++ src/assets/components/ChanceButton.jsx
@@ -2,6 +2,20 @@ import React from 'react'
 import styled from 'styled-components'
 import { motion } from 'framer-motion'
 
+const StledButton = styled(motion.button)`
+  background-color: #3498db;
+  color: #fff;
+  padding: 10px 20px;
+  font-size: 16px;
+  border-radius: 5px;
+  cursor: ${({ isLoading }) => (isLoading ? 'not-allowed' : 'pointer')};
+  box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
+
+  &:hover {
+    background-color: ${({ isLoading }) => (isLoading ? '#3498db' : '#2980b9')};
+  }
+`
+
 const ChanceButton = () => {
   return (
     <div>ChanceButton</div>
