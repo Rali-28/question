a47f2bbe8c346f45d595c730f0892c37ff10e6d4
Rali <113418936+Rali-28@users.noreply.github.com>
2023-11-26 17:09:09 +0800

    Create loader style

--- src/assets/components/Loader.jsx
+++ src/assets/components/Loader.jsx
@@ -1,5 +1,19 @@
 import React from 'react'
-import styled from 'styled-components';
+import styled from 'styled-components'
+
+const StyledLoader = styled.div`
+    border: 4px solid rgba(0, 0, 0, 0.1)
+    border-top: 4px solid #3498db
+    border-radius 50%
+    width: 20px
+    height: 20px
+    animation: spin 1s linear inifinite;
+
+    @keyframes spin {
+        0% { transform: rotate(0deg) }
+        100% ( transfor: rotate(360deg) )
+    }
+`
 
 const Loader = () => {
   return (
