0d2917d24487d40c532d42f06e7289237116ae97
Rali <113418936+Rali-28@users.noreply.github.com>
2023-11-26 16:36:02 +0800

    Create components
    Create ChanceButton and Chance Message Component

---
+++ src/assets/components/ChanceButton.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import styled from 'styled-components'
+import { motion } from 'framer-motion'
+
+const ChanceButton = () => {
+  return (
+    <div>ChanceButton</div>
+  )
+}
+
+export default ChanceButton
---
+++ src/assets/components/ChanceMessage.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ChanceMessage = () => {
+  return (
+    <div>ChanceMessage</div>
+  )
+}
+
+export default ChanceMessage
