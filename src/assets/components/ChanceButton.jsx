bc3619af2bf82199110db9f879e80b3679c7e5d2
Rali <113418936+Rali-28@users.noreply.github.com>
2023-11-26 17:07:10 +0800

    Remove Semicolons

--- src/assets/components/ChanceButton.jsx
+++ src/assets/components/ChanceButton.jsx
@@ -3,16 +3,16 @@ import styled from 'styled-components'
 import { motion } from 'framer-motion'
 
 const StledButton = styled(motion.button)`
-  background-color: #3498db;
-  color: #fff;
-  padding: 10px 20px;
-  font-size: 16px;
-  border-radius: 5px;
-  cursor: ${({ isLoading }) => (isLoading ? 'not-allowed' : 'pointer')};
-  box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
+  background-color: #3498db
+  color: #fff
+  padding: 10px 20px
+  font-size: 16px
+  border-radius: 5px
+  cursor: ${({ isLoading }) => (isLoading ? 'not-allowed' : 'pointer')}
+  box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1)
 
   &:hover {
-    background-color: ${({ isLoading }) => (isLoading ? '#3498db' : '#2980b9')};
+    background-color: ${({ isLoading }) => (isLoading ? '#3498db' : '#2980b9')}
   }
 `
 
