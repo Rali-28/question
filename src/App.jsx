7b84682bd8dfbd721d2abf3a0e226d0d22f3fa4b
Rali <113418936+Rali-28@users.noreply.github.com>
2023-11-26 17:39:00 +0800

    Add styling to container

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,14 @@ import ChanceMessage from './ChanceMessage'
 import Loader from './Loader'
 import styled from 'styled-components';
 
+const StyledAppContainer = styled.div`
+  min-height: 100vh
+  display: flex
+  align-items: center
+  justify-content: center
+  background-color: #ecf0f1
+`
+
 const App = () => {
   return (
     <div>App</div>
