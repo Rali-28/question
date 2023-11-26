0bdf91c406a8b2385ce9908ad3e0189368ddaaca
Rali <113418936+Rali-28@users.noreply.github.com>
2023-11-26 17:35:13 +0800

    Import styled-components

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React , { useState } from 'react'
 import ChanceButton from './ChanceButton'
 import ChanceMessage from './ChanceMessage'
 import Loader from './Loader'
+import styled from 'styled-components';
 
 const App = () => {
   return (
