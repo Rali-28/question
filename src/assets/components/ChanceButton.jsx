c035be9d90b1596da1c700928cb38887d2b82114
Rali <113418936+Rali-28@users.noreply.github.com>
2023-11-26 16:40:54 +0800

    Create Button style

--- src/assets/components/ChanceButton.jsx
+++ src/assets/components/ChanceButton.jsx
@@ -2,6 +2,13 @@ import React from 'react'
 import styled from 'styled-components'
 import { motion } from 'framer-motion'
 
+const Button = styled()
+`
+  @apply px-4 py-2 text-white rounded cursor-pointer
+
+  ${({ isLoading }) => isLoading && '@apply bg-gray-400 cursor-not-allowed'}
+`
+
 const ChanceButton = () => {
   return (
     <div>ChanceButton</div>
