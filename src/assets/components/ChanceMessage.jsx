b2d8a799a67695543f585879e1a8a12d06be1669
Rali <113418936+Rali-28@users.noreply.github.com>
2023-11-26 17:12:29 +0800

    Add variants for animation

--- src/assets/components/ChanceMessage.jsx
+++ src/assets/components/ChanceMessage.jsx
@@ -2,6 +2,11 @@ import React from 'react'
 import styled from 'styled-components';
 import { motion } from 'framer-motion';
 
+const messageVariants = {
+  hidden: {opacity: 0, y: -20},
+  visible: {opacity: 1, y: 0, transition: {duration: 0.5}}
+}
+
 const ChanceMessage = () => {
   return (
     <div>ChanceMessage</div>
